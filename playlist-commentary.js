9538feb8eeb3e77c270488fecc017fb90022143b
Thoth Industries <teach.the.conflicts@gmail.com>
2026-04-05 20:02:35 -0500

    Update playlist-commentary.js

--- playlist-commentary.js
+++ playlist-commentary.js
@@ -145,7 +145,7 @@ function init(playlistId, initialVideoId) {
       if (initialVideoId) {
   setTimeout(function() {
     onTrack(initialVideoId);
-  }, 2000);
+  }, 3000);
 //}
 //      if (initialVideoId){
 //        onTrack(initialVideoId);
